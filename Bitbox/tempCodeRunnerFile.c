d3455e1f91258859725b3af02264154c370d1e5a
Muhammad Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2026-04-02 01:45:48 +0600

    ok

---
+++ Bitbox/tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+//Convert Miles to Kilometers (KM)
+#include <stdio.h>
+int main(){
+    float distance_mile = 10;
+    float distance_km = distance_mile* 1.60934;
+    printf("Distance KM  %.2f\n",distance_km);
+    return 0;
+}
