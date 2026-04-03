d755e74b8f78f07197adbc17182951647937b815
Muhammad Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2026-04-03 20:32:24 +0600

    update

--- Bitbox/task1.c
+++
@@ -1,6 +0,0 @@
-//Print Hello World
-#include <stdio.h>
-int main(){
-    printf("Hello World");
-    return 0;
-}
--- Bitbox/task2.c
+++
@@ -1,9 +0,0 @@
-//Add two numbers
-#include <stdio.h>
-int main(){
-    int a=1;
-    int b=2;
-    int sum=a+b;
-    printf("Sum %d\n",sum);
-    return 0;
-}
